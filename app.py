b5c36a1104f63ff88dbc550819c696039f4a6802
ColinGreenleaf <colingleaf@gmail.com>
2024-04-13 14:20:58 -0400

    prepare route

--- app.py
+++ app.py
@@ -12,3 +12,7 @@ def index():
 @app.route('/detail', methods=['GET', 'POST'])
 def detail():
     pass
+
+@app.route('/prepare', methods=['GET', 'POST'])
+def prepare():
+    return render_template('detail.html')
