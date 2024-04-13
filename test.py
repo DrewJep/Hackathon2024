69ff127d436dcfca178cf5e20ddf38569afcab31
Drew <drewjep@comcast.net>
2024-04-13 14:00:55 -0400

    finished api fucntionality

--- test.py
+++ test.py
@@ -1,43 +1,52 @@
 import requests
 import json
 
-def write_to_file(response):
-    file_path = "data.json"
-        
+def write_to_file(response,name):
+    file_path = f"{name}.json"
     # Open the file in write mode
     with open(file_path, "w") as file:
         # Write the JSON data to the file
         json.dump(response, file, indent=4)
 
-# def query_resort(mountain):
-#     url = f"https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort/{mountain}"
+def query_resort(mountain):
+    url = f"https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort/{mountain}"
 
-#     headers = {
-#         "X-RapidAPI-Key": "6eeb8d061dmsh2aaa343775263eep1e5f01jsn5ff8f6bd7688",
-#         "X-RapidAPI-Host": "ski-resorts-and-conditions.p.rapidapi.com"
-#     }
+    headers = {
+        "X-RapidAPI-Key": "6eeb8d061dmsh2aaa343775263eep1e5f01jsn5ff8f6bd7688",
+        "X-RapidAPI-Host": "ski-resorts-and-conditions.p.rapidapi.com"
+    }
 
-#     response = requests.get(url, headers=headers)
-#     return response
+    response = requests.get(url, headers=headers)
+    return response
 
-# response = query_resort('whistler-blackcomb')
-# write_to_file(response.json())
+def query_resort_all():
+    url = "https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort"
 
-# print(response.json()["data"]["location"]['latitude'])
-
-url = "https://ski-resorts-and-conditions.p.rapidapi.com/v1/resort"
-
-headers = {
-	"X-RapidAPI-Key": "6eeb8d061dmsh2aaa343775263eep1e5f01jsn5ff8f6bd7688",
-	"X-RapidAPI-Host": "ski-resorts-and-conditions.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers)
-
-write_to_file(response.json())
+    headers = {
+        "X-RapidAPI-Key": "6eeb8d061dmsh2aaa343775263eep1e5f01jsn5ff8f6bd7688",
+        "X-RapidAPI-Host": "ski-resorts-and-conditions.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers)
+    return response
 
 
-# def query_weather(long, lat):
-#     url = https://api.weather.gov/points/39.7456,-97.0892
-#     response = requests.get(url)
-#     print()
+def query_weather(lat, long):
+    url = f'https://api.weather.gov/points/{lat},{long}'
+    response = requests.get(url)
+    office=response.json()['properties']['gridId']
+    gridX=response.json()['properties']['gridX']
+    gridY=response.json()['properties']['gridY']
+
+    url = f'https://api.weather.gov/gridpoints/{office}/{gridX},{gridY}/forecast'
+    response = requests.get(url)
+    return response
+
+############################## MAIN ##############################
+def query(mountian):
+    response = query_resort(mountian)
+    write_to_file(response.json(),f'{mountian}')
+
+    response = query_weather(response.json()["data"]["location"]['latitude'],response.json()["data"]["location"]['longitude'])
+    write_to_file(response.json(),f'{mountian}-weather')
+    print()
+query("bolton-valley")
